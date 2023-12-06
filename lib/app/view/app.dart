f0c8881b210cfb1aa04f89c4001f619a72f27abf
Denzel Gatugu <denzelgatugu@outlook.com>
2023-12-06 19:55:10 +0300

    Setup app theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:locator_app/l10n/l10n.dart';
 import 'package:locator_app/router/router.dart';
+import 'package:locator_app/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,12 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.lightTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       initialRoute: AppRouter.homeRoute,
---
+++ lib/constants/ui.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kCustomLimeGreen = Color(0xFFADFF2F);
+const kCustomGrey = Color(0xFFF2F2F2);
+const kCustomBlack = Color(0XFF0A0A0A);
