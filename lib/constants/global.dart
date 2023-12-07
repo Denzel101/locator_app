4c7534af74b52d80b8332f531360f477196e88c0
Denzel Gatugu <denzelgatugu@outlook.com>
2023-12-07 16:22:48 +0300

    Setup network service

---
+++ lib/constants/global.dart
@@ -0,0 +1,28 @@
+class OnSpaceValues {
+  OnSpaceValues({
+    required this.urlScheme,
+    required this.baseDomain,
+    required this.hiveBoxKey,
+  });
+
+  final String urlScheme;
+  final String baseDomain;
+  final String hiveBoxKey;
+
+  String get baseUrl => '$urlScheme://$baseDomain/api';
+
+  String get hiveBoxEncryptionKey => 'locator_app _${hiveBoxKey}_key';
+}
+
+class OnSpaceConfig {
+  factory OnSpaceConfig({required OnSpaceValues values}) {
+    return _instance ??= OnSpaceConfig._internal(values);
+  }
+
+  OnSpaceConfig._internal(this.values);
+
+  final OnSpaceValues values;
+  static OnSpaceConfig? _instance;
+
+  static OnSpaceConfig? get instance => _instance;
+}
